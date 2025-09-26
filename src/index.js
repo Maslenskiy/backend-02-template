663f00ca144629771b53228cd6dca0ab9452e64b
Yuriy Maslenskiy <maslenski2000@mail.ru>
2025-09-26 18:59:38 +0300

    создал сервер и сделал несколько обработчиков

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const getUsers = require('./modules/users')
+const PORT = process.env.PORT || 3000
+const HOST = 'http://127.0.0.1'
 
 const server = http.createServer((request, response) => {
-
     // Написать обработчик запроса:
     // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
     // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
@@ -9,4 +11,51 @@ const server = http.createServer((request, response) => {
     // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
     // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
 
+
+      const url = new URL(request.url, HOST)
+    const userName = url.searchParams.get('hello')
+
+      if (userName) {
+        response.statusCode = 200
+        response.statusMessage = 'ok'
+        response.setHeader('Content-Type', 'text/plain')
+        response.write(`Hello, ${userName}.`)
+        response.end()
+        return
+      }
+
+      if (request.url === '/?hello') {
+        response.statusCode = 400
+        response.statusMessage = 'Bad Request'
+        response.setHeader('Content-Type', 'text/plain')
+        response.write(`Enter a name`)
+        response.end()
+        return
+      }
+
+      if (request.url === `/?users`) {
+        response.statusCode = 200
+        response.statusMessage = 'ok'
+        response.setHeader('Content-Type', 'application/json')
+        response.write(getUsers())
+        response.end()
+        return
+      }
+
+      if (request.url === '/') {
+        response.statusCode = 200
+        response.statusMessage = 'ok'
+        response.setHeader('Content-Type', 'text/plain')
+        response.write(`Hello, World!`)
+        response.end()
+        return
+      }
+
+      response.statusCode = 500
+      response.statusMessage = 'Internal Server Error'
+      response.end()
 });
+
+server.listen(PORT, () => {
+    console.log(`Сервер запущен по адресу: ${HOST}:${PORT}`)
+})
